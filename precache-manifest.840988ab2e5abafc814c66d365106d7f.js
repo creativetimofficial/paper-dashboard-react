635fe0ebe37d89c900b532f6c13044127e0195e7
Manu <emanuelnazare@yahoo.ro>
2018-10-09 20:14:43 +0300

    Updates

---
+++ precache-manifest.840988ab2e5abafc814c66d365106d7f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "5cb2b9c1cc02caa4747d",
+    "url": "/static/css/main.fd0fd9ec.chunk.css"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "c8d7fbfd2c482487fe29",
+    "url": "/static/js/1.c8d7fbfd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "5cb2b9c1cc02caa4747d",
+    "url": "/static/js/main.5cb2b9c1.chunk.js"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "c8d7fbfd2c482487fe29",
+    "url": "/static/css/1.0cbc392f.chunk.css"
+  },
+  {
+    "revision": "316aeb2252a431d0eadde6d50df9521b",
+    "url": "/index.html"
+  }
+];
