d0d8a431ba1102a1210a38690091ab6ab296956f
Manu <emanuelnazare@yahoo.ro>
2018-10-11 14:26:25 +0300

    - Added meta tags, google analytics and facebook pixel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createHashHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
+import ReactPixel from "react-facebook-pixel";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss";
@@ -10,6 +12,22 @@ import "assets/demo/demo.css";
 import indexRoutes from "routes/index.jsx";
 
 const hist = createHashHistory();
+
+ReactPixel.init("111649226022273");
+ReactPixel.pageView();
+ReactPixel.fbq("track", "PageView");
+
+ReactGA.initialize("UA-46172202-1");
+ReactGA.set({ page: window.location.pathname + window.location.search });
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+hist.listen(location => {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
+  ReactPixel.pageView();
+  ReactPixel.fbq("track", "PageView");
+});
 
 ReactDOM.render(
   <Router history={hist}>
