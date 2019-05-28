0cd77a6b28a6985c90d93867fb152e200b2e06ba
Manu <emanuelnazare@yahoo.ro>
2019-05-28 15:10:15 +0300

    - Added perfect-scrollbar css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss?v=1.1.0";
 import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 import AdminLayout from "layouts/Admin.jsx";
 
