24b99df7f2619770e77c6df855ff7ef094dbf91a
Manu <emanuelnazare@yahoo.ro>
2019-05-31 10:41:51 +0300

    Updates

---
+++ precache-manifest.1116bf2b8e930787be142c8420eb673f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3370a9c622e1142f2099ede7cf60b7eb",
+    "url": "/paper-dashboard-react/index.html"
+  },
+  {
+    "revision": "6c9f0fcb71b2459d64c8",
+    "url": "/paper-dashboard-react/static/css/2.654f473f.chunk.css"
+  },
+  {
+    "revision": "3c0bce13ad0bcc4d6249",
+    "url": "/paper-dashboard-react/static/css/main.2f913413.chunk.css"
+  },
+  {
+    "revision": "6c9f0fcb71b2459d64c8",
+    "url": "/paper-dashboard-react/static/js/2.8e4356bc.chunk.js"
+  },
+  {
+    "revision": "3c0bce13ad0bcc4d6249",
+    "url": "/paper-dashboard-react/static/js/main.0fd0b3b6.chunk.js"
+  },
+  {
+    "revision": "b3fe53a14804cfc5877b",
+    "url": "/paper-dashboard-react/static/js/runtime~main.ff95ff9f.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/paper-dashboard-react/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/paper-dashboard-react/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/paper-dashboard-react/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/paper-dashboard-react/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paper-dashboard-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/paper-dashboard-react/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paper-dashboard-react/precache-manifest.e4cbff06bff0933aa33244d569fef0eb.js"
+  "/paper-dashboard-react/precache-manifest.1116bf2b8e930787be142c8420eb673f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/paper-dashboard-react/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paper-dashboard-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
