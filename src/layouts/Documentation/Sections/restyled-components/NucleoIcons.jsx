80d3c5f0862d111c5e894e198bfd240c2eaf6bd9
Manu <emanuelnazare@yahoo.ro>
2019-07-05 12:21:09 +0300

    - fb pixel and copyright

--- src/layouts/Documentation/Sections/restyled-components/NucleoIcons.jsx
+++ src/layouts/Documentation/Sections/restyled-components/NucleoIcons.jsx
@@ -1,3 +1,21 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 /*eslint-disable*/
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
