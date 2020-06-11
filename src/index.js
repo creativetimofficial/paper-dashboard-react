281402ab3df2e71c68b98db5febdc2ad28db808c
EINazare <emanuelioannazare@gmail.com>
2020-06-11 11:50:53 +0300

    fix: change and delete unused packages

--- src/index.js
+++ src/index.js
@@ -20,8 +20,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createHashHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-import ReactGA from "react-ga";
-import ReactPixel from "react-facebook-pixel";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss?v=1.2.0";
@@ -32,22 +30,6 @@ import AdminLayout from "layouts/Admin.js";
 import DocumentationLayout from "layouts/Documentation/Documentation.js";
 
 const hist = createHashHistory();
-
-ReactPixel.init("111649226022273");
-ReactPixel.pageView();
-ReactPixel.fbq("track", "PageView");
-
-ReactGA.initialize("UA-46172202-1");
-ReactGA.set({ page: window.location.pathname + window.location.search });
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-hist.listen(location => {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-
-  ReactPixel.pageView();
-  ReactPixel.fbq("track", "PageView");
-});
 
 ReactDOM.render(
 <Router history={hist}>
