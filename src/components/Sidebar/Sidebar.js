e7f9b1ce4f76c1bab10715792960e6b8e17d9187
EINazare <emanuelioannazare@gmail.com>
2021-05-17 11:46:17 +0300

    refactor: change class components to function ones

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -26,79 +26,72 @@ import logo from "logo.svg";
 
 var ps;
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.activeRoute.bind(this);
-    this.sidebar = React.createRef();
-  }
+function Sidebar(props) {
+  const sidebar = React.useRef();
   // verifies if routeName is the one active (in browser input)
-  activeRoute(routeName) {
-    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
-  }
-  componentDidMount() {
+  const activeRoute = (routeName) => {
+    return props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  };
+  React.useEffect(() => {
     if (navigator.platform.indexOf("Win") > -1) {
-      ps = new PerfectScrollbar(this.sidebar.current, {
+      ps = new PerfectScrollbar(sidebar.current, {
         suppressScrollX: true,
         suppressScrollY: false,
       });
     }
-  }
-  componentWillUnmount() {
-    if (navigator.platform.indexOf("Win") > -1) {
-      ps.destroy();
-    }
-  }
-  render() {
-    return (
-      <div
-        className="sidebar"
-        data-color={this.props.bgColor}
-        data-active-color={this.props.activeColor}
-      >
-        <div className="logo">
-          <a
-            href="https://www.creative-tim.com"
-            className="simple-text logo-mini"
-          >
-            <div className="logo-img">
-              <img src={logo} alt="react-logo" />
-            </div>
-          </a>
-          <a
-            href="https://www.creative-tim.com"
-            className="simple-text logo-normal"
-          >
-            Creative Tim
-          </a>
-        </div>
-        <div className="sidebar-wrapper" ref={this.sidebar}>
-          <Nav>
-            {this.props.routes.map((prop, key) => {
-              return (
-                <li
-                  className={
-                    this.activeRoute(prop.path) +
-                    (prop.pro ? " active-pro" : "")
-                  }
-                  key={key}
-                >
-                  <NavLink
-                    to={prop.layout + prop.path}
-                    className="nav-link"
-                    activeClassName="active"
-                  >
-                    <i className={prop.icon} />
-                    <p>{prop.name}</p>
-                  </NavLink>
-                </li>
-              );
-            })}
-          </Nav>
-        </div>
+    return function cleanup() {
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+      }
+    };
+  });
+  return (
+    <div
+      className="sidebar"
+      data-color={props.bgColor}
+      data-active-color={props.activeColor}
+    >
+      <div className="logo">
+        <a
+          href="https://www.creative-tim.com"
+          className="simple-text logo-mini"
+        >
+          <div className="logo-img">
+            <img src={logo} alt="react-logo" />
+          </div>
+        </a>
+        <a
+          href="https://www.creative-tim.com"
+          className="simple-text logo-normal"
+        >
+          Creative Tim
+        </a>
       </div>
-    );
-  }
+      <div className="sidebar-wrapper" ref={sidebar}>
+        <Nav>
+          {props.routes.map((prop, key) => {
+            return (
+              <li
+                className={
+                  activeRoute(prop.path) + (prop.pro ? " active-pro" : "")
+                }
+                key={key}
+              >
+                <NavLink
+                  to={prop.layout + prop.path}
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  <i className={prop.icon} />
+                  <p>{prop.name}</p>
+                </NavLink>
+              </li>
+            );
+          })}
+        </Nav>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
