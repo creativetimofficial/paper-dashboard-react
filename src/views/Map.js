c650ec8ca4f62cdf9e2e24b0f57771a771762467
EINazare <emanuelioannazare@gmail.com>
2021-05-17 11:46:18 +0300

    refactor: change class components to function ones

--- src/views/Map.js
+++ src/views/Map.js
@@ -194,31 +194,29 @@ const MapWrapper = () => {
   );
 };
 
-class Map extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="content">
-          <Row>
-            <Col md="12">
-              <Card>
-                <CardHeader>Google Maps</CardHeader>
-                <CardBody>
-                  <div
-                    id="map"
-                    className="map"
-                    style={{ position: "relative", overflow: "hidden" }}
-                  >
-                    <MapWrapper />
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </>
-    );
-  }
+function Map() {
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col md="12">
+            <Card>
+              <CardHeader>Google Maps</CardHeader>
+              <CardBody>
+                <div
+                  id="map"
+                  className="map"
+                  style={{ position: "relative", overflow: "hidden" }}
+                >
+                  <MapWrapper />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
 }
 
 export default Map;
